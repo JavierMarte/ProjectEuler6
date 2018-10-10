b12afe3846c4daabeb0e1cc0132be799a239f713
Javier Marte <kaison54@hotmail.com>
2018-10-10 11:20:46 -0400

    Add files via upload

---
+++ projectEuler6.java
@@ -0,0 +1,30 @@
+package googleInterview;
+
+public class projectEuler6 {
+
+	public static int sum(int limit){
+		
+		int i = 1;
+		int sumOne = 0;
+		int sumTwo = 0;
+		
+		while(i <= limit){
+			
+			sumOne = (i * i) + sumOne;
+			
+			sumTwo += i;
+			i++;
+		}
+		
+		sumTwo = sumTwo * sumTwo;
+		
+		return (sumTwo - sumOne);
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println(sum(10));
+	}
+
+}
